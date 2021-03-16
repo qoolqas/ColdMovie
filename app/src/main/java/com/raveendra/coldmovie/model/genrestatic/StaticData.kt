bf3457a35ad6381331998a2ad074371515d1234e
qoolqas <qoolqas@gmail.com>
2021-03-16 17:50:43 +0700

    connection and model ThemovieDB API

---
+++ app/src/main/java/com/raveendra/coldmovie/model/genrestatic/StaticData.kt
@@ -0,0 +1,18 @@
+package com.raveendra.coldmovie.model.genrestatic
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+data class GenreStaticResponse(
+    val data: List<StaticData>? = null
+)
+
+@Parcelize
+data class StaticData(
+    val image: Int? = null,
+    val name: String? = null,
+    val id: Int? = null,
+    val kode: String? = null
+): Parcelable
+
